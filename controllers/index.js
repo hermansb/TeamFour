6b965a738ee345b883186d9216e03e1af5b902a3
hermanba <badwellh@hermansbTP.torolab.ibm.com>
2014-11-03 15:43:32 -0500

    twilio support

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
-
+// Twilio Credentials 
+var accountSid = 'AC3c2fa9b2734379e8254c2b4e938b7c6e'; 
+var authToken = '36a402403a5cb246162bcb4b22a50f2f'; 
+ 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+ 
 var IndexModel = require('../models/index');
 
 
@@ -14,5 +20,18 @@ module.exports = function (router) {
         res.render('login', model);
         
     });
+	
+	
+router.get('/sendtext', function (req, res) {
+	client.messages.create({ 
+		to: "6479091174", 
+		from: "+16475600524", 
+		body: "Did you get this?",   
+	}, function(err, message) { 
+		console.log(message.sid); 
+	});
+
+}
+
 
 };
