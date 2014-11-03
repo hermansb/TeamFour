7539290736eefae15c62a6e45f4e7cc7b2b5e66e
hermanba <badwellh@hermansbTP.torolab.ibm.com>
2014-11-03 18:20:02 -0500

    twilio working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var client = require('twilio')('AC3c2fa9b2734379e8254c2b4e938b7c6e', '36a402403a5cb246162bcb4b22a50f2f'); 
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/sendtext', function (req, res) {
+		client.messages.create({ 
+			to: "6479091164", 
+			from: "+16475600524", 
+			body: "Did you get this?",   
+		}, function(err, message) { 
+			if (err) {
+				console.log('there was an err' + JSON.stringify(err));
+			}
+			else {
+				//console.log(message); 
+				res.send('Text sent! Congratz');
+			}
+		});
+		
+	});
 
 module.exports = router;
