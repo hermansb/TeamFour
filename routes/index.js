56836e2202084288024d8c796f5d9bd44ab78301
hermanba <hermanba@ca.ibm.com>
2014-11-04 15:19:11 -0500

    Using Randalls Twilio account

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var client = require('twilio')('AC3c2fa9b2734379e8254c2b4e938b7c6e', '36a402403a5cb246162bcb4b22a50f2f'); 
+var client = require('twilio')('AC35793279f02cec99521ee00cbcc95a45', '729cc5f50620ea888bc922691066192c'); 
 var nano = require('nano')('https://ada12f18-1a96-412b-be06-55caa0cf0d9c-bluemix:bafd91061af5bf97cfb7d76912bfdef5f2bbc22f50ed574137bf8bda5d22a711@ada12f18-1a96-412b-be06-55caa0cf0d9c-bluemix.cloudant.com');
 var passport = require('passport');
 var passwordCheck = require('password-hash-and-salt');
