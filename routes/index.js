bbf449ea1f2a1627f0463c6ee9f81565533add32
nhtan <hientann@ca.ibm.com>
2014-11-03 19:27:26 -0500

    add route protection

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/sendtext', function (req, res) {
+router.get('/sendtext', isLoggedIn, function (req, res) {
 		client.messages.create({ 
 			to: "6479091164", 
 			from: "+16475600524", 
